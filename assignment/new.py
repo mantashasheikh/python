ff84dc4e3bfc0195dcdbc8b06eb8be74ec7b0268
mantashasheikh <mantashasheikh95844@gmail.com>
2025-07-01 11:26:59 +0530

    python starts again

---
+++ assignment/new.py
@@ -0,0 +1,56 @@
+#syntax to print output
+print("hello world")
+
+#this is a comment
+
+#variables
+x = 5
+y = "john"
+print(x)
+print(y)
+
+#casting
+a = str(3)
+b = int(3)
+c = float(3)
+print(a)
+print(b)
+print(c)
+
+#get the type
+print(type(x))
+print(type(y))
+
+#tells the memory address
+d = 2.34
+print(id(d))
+
+#python operators
+#practice questions
+#area of rectangle
+length = 6
+width = 3
+area = length * width
+print(area)
+
+#area of triangle
+base = 3
+height = 6
+areaOfTriangle = 0.5*base*height
+print(areaOfTriangle)
+
+#area of circle
+radius = 2
+areaOfCircle = 3.14*radius*radius
+print(areaOfCircle)
+
+#celcius to fahrenheit
+celcius = 45
+fahrenheit = (celcius*9/5)+32
+print(fahrenheit)
+
+
+
+
+
+
