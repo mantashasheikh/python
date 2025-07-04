d2a9909216e50c153901597e1487bdb08513a234
mantashasheikh <mantashasheikh95844@gmail.com>
2025-07-04 23:15:28 +0530

    python list

--- assignment/new.py
+++ assignment/new.py
@@ -49,6 +49,8 @@ celcius = 45
 fahrenheit = (celcius*9/5)+32
 print(fahrenheit)
 
+
+
 #string
 print("hello world")
 print('hello world')
@@ -110,6 +112,126 @@ print(t.count("is"))
 print(t.endswith("powerful"))
 print(t.find("hero"))
 print(t.startswith("tony"))
+
+
+#python booleans
+print(10>2)
+print(10==9)
+print(10<9)
+
+
+print(bool("hello"))
+print(bool(15))
+print(bool(["apple","mango","banana"]))
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(()))
+print(bool(""))
+print(bool([]))
+print(bool({}))
+
+
+#python list
+thislist = ["apple","mango","banana","apple"]
+print(thislist)
+print(len(thislist))
+print(type(thislist))
+
+print(thislist[1])
+print(thislist[-1])
+print(thislist[1:4])
+print(thislist[-4:-1])
+
+if "apple" in thislist:
+    print("yes, apple is present in thislist")
+
+thislist[1] = "blackcurrent"
+print(thislist)   
+
+thislist[1:3] = ["blackcurrent","watermelon"]
+print(thislist) 
+
+thislist.insert(2,"watermelon")
+print(thislist)
+
+list = ["tomato","potato","brinjal","ladyfinger"]
+print(len(list))
+list.append("pumpkin")
+print(list)
+print(len(list))
+
+list.insert(1,"onion")
+print(list)
+print(len(list))
+
+thislist = ["apple","mango","banana"]
+tropical = ["cherry","pineapple","papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+thistuple = ("kiwi","orange")
+thislist.extend(thistuple)
+print(thislist)
+
+list.remove("brinjal")
+print(list)
+
+list.pop(1)
+print(list)
+
+del list[0]
+print(list)
+
+del list
+print(list)
+
+name = ["mantasha","rimsha","umra","iqra","bushra"]
+name.clear()
+print(name)
+
+name = ["mantasha","rimsha","umra","iqra","bushra"]
+name.sort()
+print(name)
+
+name.sort(reverse=True)
+print(name)
+
+city = ["Bhopal","delhi","Mumbai","Kolkata"]
+city.sort()
+print(city)
+
+city.reverse()
+print(city)
+
+mylist = city.copy()
+print(mylist)
+
+mylist = list(name)
+print(mylist)
+
+list1 = ["a","b","c"]
+list2 = [1,2,3]
+list3 = list1 +list2
+print(list3)
+
+list4 = [7,8,9]
+for x in list4:
+    list1.append(x)
+print(list1)
+
+list5 = [12,23,34]
+list1.extend(list2)
+print(list1)    
+
+
+
+
+
+
+
+
 
 
 
