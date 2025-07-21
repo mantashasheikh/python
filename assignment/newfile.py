6a2e572ebb27bb7764377df2bdceab2c21a12e23
mantashasheikh <mantashasheikh95844@gmail.com>
2025-07-21 13:17:16 +0530

    string method

---
+++ assignment/newfile.py
@@ -0,0 +1,17 @@
+s = "this is the python class"
+print(s.lower())
+print(s.upper())
+print(s.capitalize())
+print(s.title())
+print(s.index("p"))
+print(s.count("s"))
+print(s.find("P"))
+s1 = "python"
+s2 = "language"
+s3 = ' '.join([s1,s2])
+print(s3)
+print(type(s))
+
+s4 = "this is a python class"
+res = s.split()
+print(res)
