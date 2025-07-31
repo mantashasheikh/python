3382a85bf9f1c3440702a7ebdd968ee03a371537
mantashasheikh <mantashasheikh95844@gmail.com>
2025-07-31 13:42:42 +0530

    pattern

--- assignment/pattern.py
+++ assignment/pattern.py
@@ -31,13 +31,46 @@
 # i = 5
 # while i>=1:
 #     print("*"*i)
-#     i = i-1  
+#     i = i-1 
+ 
+    
+# n = int(input("enter a number:"))
+# i = 0
+# while i<n:
+#     print("*"*(n-i))
+#     i = i+1  
+    
+    
+# n = int(input("enter a number:"))
+# i = 0
+# while i<n:
+#     print(" "*i + "*"*(n-i))
+#     i = i+1 
+    
+    
+# n = int(input("enter a number:"))
+# i = 0
+# while i<n:
+#     print(" "*i + "_*"*(n-i))
+#     i = i+1
+    
     
 n = int(input("enter a number:"))
+n = int(input("enter a number:"))
+i = 1
+while i<=n:
+    print(" "*(n-i) + "_*"*i)
+    i = i+1
+
 i = 0
 while i<n:
-    print("*"*(n-i))
-    i = i+1         
+    print(" "*i + "_*"*(n-i))
+    i = i+1     
+    
+    
+        
+    
+               
     
 
     
