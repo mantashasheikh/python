ddd4beb8830afefa7d2f818c7b7d70e55f5577d8
mantashasheikh <mantashasheikh95844@gmail.com>
2025-08-04 11:30:33 +0530

    python manual question

--- assignment/manual.py
+++ assignment/manual.py
@@ -228,11 +228,42 @@
 # print(rev)
 
 
-l = [1,2,3,4,5] 
-rev = []
-for i in range(len(l)-1,-1,-1):
-    rev += l[i]
-print(rev)
+
+
+# check the given list is palindrome or not
+l =  [1,0,1]
+rev = (l[::-1])
+if l==rev:
+    print("palindrome")
+else:
+    print("not a palindrome")    
+
+
+
+
+
+
+# sort the list
+# l = [2,1,5,4,7,6,3] 
+# for i in range(len(l)):
+#     for j in range(i+1,len(l)):
+#         if l[i] > l[j]:
+#             l[i],l[j] = l[j],l[i]
+# print(l)   
+
+
+
+
+         
+
+
+
+
+
+
+
+
+
 
 
 
