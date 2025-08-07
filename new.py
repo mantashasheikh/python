3a27055b6e4169e13cd9401b46d272aad735a271
mantashasheikh <mantashasheikh95844@gmail.com>
2025-08-07 22:05:46 +0530

    python manual question

--- new.py
+++ new.py
@@ -33,13 +33,96 @@
 #     print("*"*i) 
     
 
-t = (2,4,6,"a","b","c")
-value = input("enter a value : ")      #error
-index = 0
-for item in t:
-    if item == value:
-        print(index)
-    index+=1   
+# t = (2,4,6,"a","b","c")
+# value = input("enter a value : ")      #error
+# index = 0
+# for item in t:
+#     if item == value:
+#         print(index)
+#     index+=1 
+ 
+# n = int(input("enter a number : "))
+
+# for i in range(1, n+1):
+#     if i==4 or i==7:
+#         pass
+#     print(i) 
+    
+
+# n = int(input("enter a number : "))
+
+# for i in range(1, n+1):
+#     if i==4 or i==7:
+#         continue
+#     print(i) 
+    
+    
+# n = int(input("enter a number : "))
+
+# for i in range(1, n+1):
+#     if i==4 or i==7:
+#         break
+#     print(i)   
+    
+    
+# n = int(input("enter a number : "))
+# i=1
+# while i<=n:
+#     if i==4 or i==7:
+#         pass
+#     else:
+#         print(i)
+#     i = i+1
+     
+    
+    
+# n = int(input("enter a number : "))   #drawback 
+# i=1
+# while i<=n:
+#     if i==4 or i==7:
+#         print("hello")
+#         continue
+#     else:
+#         print(i)
+#     i = i+1  
+
+
+
+    
+# n = int(input("enter a number : "))   
+# i=1
+# while i<=n:
+#     if i==4 or i==7:
+#         i=i+1
+#         print("hello")
+#         continue
+#     else:
+#         print(i)
+#      
+    
+    
+# n = int(input("enter a number : "))   
+# i=1
+# while i<=n:
+#     if i==4 or i==7:
+        
+#         print("hello")
+#         pass
+#     else:
+#         print(i)
+#     i = i+1 
+    
+    
+
+
+print(print())
+x = print()
+print(x)
+print(x)
+print(x)  
+print(print("hello"))                 
+                        
+    
     
 
 
