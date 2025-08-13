58076bf9a603abe60f6905e44212b8b6f2d14345
mantashasheikh <mantashasheikh95844@gmail.com>
2025-08-13 22:34:31 +0530

    python function

--- assignment/func.py
+++ assignment/func.py
@@ -58,31 +58,91 @@
 
 
 # keyword positional argument
-def value(x,y,z):
-    print(f"value of x = {x}") 
-    print(f"value of x = {y}") 
-    print(f"value of x = {z}") 
-result = value(z=10,y=20,x=30)
+# def value(x,y,z):
+#     print(f"value of x = {x}") 
+#     print(f"value of x = {y}") 
+#     print(f"value of x = {z}") 
+# result = value(z=10,y=20,x=30)
 
 
-def add(x,y,z):
-   return x+y+z 
-result = add(z=10,y=20,x=30)
-print(result)
+# def add(x,y,z):
+#    return x+y+z 
+# result = add(z=10,y=20,x=30)
+# print(result)
 
 
 
 # default keyword argument
-def sum(x=0,y=0,z=0):
-    return x+y+z
-result = sum(z=10,y=20,x=30)
-print(result)
-result = sum(z=10,y=20)
-print(result)
+# def sum(x=0,y=0,z=0):
+#     return x+y+z
+# result = sum(z=10,y=20,x=30)
+# print(result)
+# result = sum(z=10,y=20)
+# print(result)
 
 
 
 # variable length keyword argument
+
+
+
+# eval
+# x = input("enter a number : ")
+# print(x)
+# print(type(x))
+
+# print(tuple(x))
+
+# y = (10,20,30)
+# print(str(y))
+
+
+# x = eval(input("enter a number : "))
+# print(x)
+# print(type(x))
+
+# y=eval('2+5-1*5')
+# print(y)
+
+
+# x = ((1,2,3,4,5),)
+# sum = 0
+# for i in x:
+#     print(i)
+#     for j in i:
+#         sum+=j
+# print(sum)  '
+
+
+# x = eval(input("enter a number : "))
+# print(x)
+# print(type(x))   
+
+
+# def add(*n):
+#     sum = 0
+#     for i in n:
+#        print(i)
+#     for j in i:
+#         sum+=j
+#     return sum
+# ans = add((2,3,4,5),)
+# print(ans)
+
+
+
+# def display(**n):
+#     for i in n:
+#         for k,v in n.items():
+#             print(f'key is ${k} and value is ${v}')
+# x = eval(input("enter a tuple : "))
+# result = display(x)
+# print(result)            
+       
+       
+
+       
+
 
 
 
