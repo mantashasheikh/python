2e5b9a81ef193efb5581d003760a7908be7de52c
mantashasheikh <mantashasheikh95844@gmail.com>
2025-08-16 13:49:28 +0530

    python manual question 2

--- assignment/manual2.py
+++ assignment/manual2.py
@@ -237,10 +237,99 @@
 #     * * * 
 #    * * * * 
 #   * * * * * 
-n=int(input("Enter the number of rows: "))  
-for i in range(1,n+1):  
-    print(" "*(n-i)," *"*i)    
+# n=int(input("Enter the number of rows: "))  
+# for i in range(1,n+1):  
+#     print(" "*(n-i)," *"*i)  
     
+    
+    
+
+#  ***** 
+#   **** 
+#    *** 
+#     ** 
+#      * 
+# n=int(input("Enter the number of rows: "))  
+# for i in range(n,0,-1):  
+#     print(" "*(n-i),"*"*i)
+    
+    
+    
+# *****  
+# **** 
+# *** 
+# ** 
+# * 
+# n=int(input("Enter the number of rows: "))  
+# for i in range(n,0,-1):  
+#     print("*"*i) 
+    
+    
+    
+    
+#   * * * * * 
+#    * * * * 
+#     * * * 
+#      * * 
+#       * 
+# n=int(input("Enter the number of rows: "))  
+# for i in range(n,0,-1):  
+#     print(" "*(n-i)," *"*i)                
+    
+    
+    
+
+#       * 
+#      * * 
+#     * * * 
+#    * * * * 
+#   * * * * * 
+#    * * * * 
+#     * * * 
+#      * * 
+#       * 
+# n=int(input("Enter the number of rows: ")) 
+# for i in range(1,n+1):  
+#     print(" "*(n-i),"* "*i)  
+# m=n-1 
+# for i in range(m,0,-1):  
+#     print(" "*(m-i)," *"*i) 
+
+
+    
+    
+#      *
+#     ** 
+#    *** 
+#   ****
+#  ***** 
+# ***** 
+# **** 
+# *** 
+# ** 
+# * 
+# n=int(input("Enter the number of rows: "))  
+# for i in range(0,n+1):  
+#     print(" "*(n-i),"*"*i) 
+# for i in range(n,0,-1):  
+#     print("*"*i," "*(n-i))
+    
+    
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+# n=int(input("Enter the number of rows: "))  
+# for i in range(0,n+1):  
+#     print("* "*i) 
+# m=n-1 
+# for i in range(m,0,-1):  
+#     print("* "*i)              
     
     
     
