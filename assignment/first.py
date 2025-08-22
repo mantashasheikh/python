be511bf6db10acbe29d67aa09bd8df43f19661c0
mantashasheikh <mantashasheikh95844@gmail.com>
2025-08-22 13:05:25 +0530

    python file

---
+++ assignment/first.py
@@ -0,0 +1,7 @@
+x = 10
+y = 20
+def add(p,q):
+    return p+q
+class calculator:
+    def add(self,x,y):
+        print(x+y)
---
+++ assignment/second.py
@@ -0,0 +1,21 @@
+# import first
+# print(first.x)
+# print(first.y)
+# r = int(input("enter a number :"))
+# s = int(input("enter a number"))
+# res = first.add(r,s)
+# obj = first.calculator()
+# obj.add(r,s)
+
+
+
+from first import x,y,add,calculator
+print(x)
+print(y)
+r = int(input("enter a number :"))
+s = int(input("enter a number"))
+res = add(r,s)
+obj = calculator()
+obj.add(r,s)
+
+
