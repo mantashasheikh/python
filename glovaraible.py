0afe6da9f5f618768154a7457520021021253315
mantashasheikh <mantashasheikh95844@gmail.com>
2025-09-01 08:57:10 +0530

    global and local variable

--- glovaraible.py
+++ glovaraible.py
@@ -1,7 +1,24 @@
-x = 10
-def new():
-    print(x)
-new()
+# x = 10
+# def new():
+#     print(x)
+# new()
+
+# x = 10
+# def new():
+#     y = 20
+#     print(x,y)
+# new()
+# print(x)
+# print(y)
+
+# x = 10
+# def new():
+#     global y
+#     y = 20
+#     print(x,y)
+# new()
+# print(x)
+# print(y)        
 
 
 
