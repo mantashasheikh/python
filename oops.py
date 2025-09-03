1b9560212e661de527bdae98c624e15739832e27
mantashasheikh <mantashasheikh95844@gmail.com>
2025-09-03 13:02:05 +0530

    oops

--- oops.py
+++ oops.py
@@ -28,13 +28,25 @@
 # print(New.__doc__)
 
 
-class New:
-    "for demo purpose"
-obj = New
-obj1 = New
-obj2 = New
-print(id(New))
-print(id(obj),id(obj1),id(obj2))
+# class New:
+#     "for demo purpose"
+# obj = New
+# obj1 = New
+# obj2 = New
+# print(id(New))
+# print(id(obj),id(obj1),id(obj2))
+
+
+class Student:
+    def __init__(self):
+        print("external constructor called")
+x = Student
+print(id(x),id(Student))    #same address
+y = Student()
+print(id(y),id(Student))    #different address
+
+
+
 
 
 
