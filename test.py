ea651a6b16269a78bf398827e43980718dd62e48
mantashasheikh <mantashasheikh95844@gmail.com>
2025-09-27 12:01:17 +0530

    python test question

--- test.py
+++ test.py
@@ -1,25 +1,59 @@
 # natural number
-n = int(input("enter a number : "))
-for i in range(1,n+1):
-    print(i , end=" ")
+# n = int(input("enter a number : "))
+# for i in range(1,n+1):
+#     print(i , end=" ")
 
 # even number
-n = int(input("enter a number : ")) 
-for i in range(n+1):
-    if i%2==0:
-        print(i)
+# n = int(input("enter a number : ")) 
+# for i in range(n+1):
+#     if i%2==0:
+#         print(i)
         
         
         
 # odd number
-n = int(input("enter a number : "))
-for i in range(n+1):
-    if i%2!=0:
-        print(i) 
+# n = int(input("enter a number : "))
+# for i in range(n+1):
+#     if i%2!=0:
+#         print(i) 
+
         
  
 # prime number
-n = int(input("enter a number : ")) 
-for i in range 
+# n = int(input("enter a number :")) 
+# print(f"Prime numbers up to {n}:")
+# for num in range(2, n+1):
+#     for i in range(2, num):
+#         if num % i == 0:
+#             break
+#     else:   
+#         print(num, end=" ") 
+
+
+
+
+# factor of a given number
+n = int(input("enter a number : "))
+for i in range(1,n+1):
+    if n%i==0:
+        print(i, end=" ")
+        
+
+# factorial
+  
+
+
+
+
+      
+        
+        
+    
+ 
+ 
+                  
+
+            
+        
         
     
