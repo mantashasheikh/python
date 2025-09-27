e4f414aba06890df0f3b114be75210b5f57e34fd
mantashasheikh <mantashasheikh95844@gmail.com>
2025-09-27 08:11:25 +0530

    python test question

---
+++ test.py
@@ -0,0 +1,12 @@
+# natural number
+n = int(input("enter a number : "))
+for i in range(1,n+1):
+    print(i , end=" ")
+
+# even number
+n = int(input("enter a number : ")) 
+for i in range(n+1):
+    if i%2==0:
+        print(i)
+        
+    
