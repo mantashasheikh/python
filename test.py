0ea93afca2dd18927c1addc308ccc1b2eae485c5
mantashasheikh <mantashasheikh95844@gmail.com>
2025-09-27 12:19:09 +0530

    python test question

--- test.py
+++ test.py
@@ -33,14 +33,47 @@
 
 
 # factor of a given number
-n = int(input("enter a number : "))
-for i in range(1,n+1):
-    if n%i==0:
-        print(i, end=" ")
+# n = int(input("enter a number : "))
+# for i in range(1,n+1):
+#     if n%i==0:
+#         print(i, end=" ")
         
 
+
+
 # factorial
-  
+# n = int(input("enter a number : "))
+# fact = 1
+# for i in range(1,n+1):
+#     fact*=i
+#     print(fact)
+    
+    
+    
+# leap year
+# year = int(input("enter a number : "))
+# if (year%4==0 and year%100!=0)or(year%400==0):
+#     print("Leap year")
+# else:
+#     print("not a leap year")
+    
+
+
+
+
+#   armstrong
+n = int(input("enter a number : "))
+temp = n
+ans = 0
+x = len(str(n))
+while n>0:
+    digit = n%10
+    ans+=digit**x
+    n//=10
+if ans==temp:
+    print("armstrong")
+else:
+    print("not an armstrong")
 
 
 
