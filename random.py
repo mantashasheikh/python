b4fec953d7e8841aa97dd519a35eceeb5afd1a02
mantashasheikh <mantashasheikh95844@gmail.com>
2025-11-23 13:17:41 +0530

    python random code

--- random.py
+++ random.py
@@ -139,3 +139,14 @@ if rev==ans:
     print("it is a palindrome")
 else:
     print("It is not a palindrome")
+    
+    
+    
+num = int(input("enter a number:"))
+for num in range(1,num+1):
+    if num>1:
+        for j in range(2,num):
+            if num%j==0:
+                break
+            else:
+                print(num)     
