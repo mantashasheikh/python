8644629c148b32f6a9a88ff291ad60f61e7a39ed
mantashasheikh <mantashasheikh95844@gmail.com>
2025-11-24 20:50:03 +0530

    python random code

--- random.py
+++ random.py
@@ -174,5 +174,12 @@ for i in range(1,n1+1):
         if(n1%i==0 and n2%i==0):
             hcf=i
             break
-print(hcf)                
+print(hcf)
+
+
+base = int(input("enter a number:"))
+power = int(input("enter a number"))
+for i in range(1, base+1):
+    ans=base**power
+print(ans)                
                 
